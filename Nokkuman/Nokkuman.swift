862d1e9ff1da3adfd6404710b827578f8fd5c6d1
ha <ha@ex.com>
2020-07-08 10:59:56 +0900

    初コミット。Nokkumanを画面に表示した。

---
+++ Nokkuman/Nokkuman.swift
@@ -0,0 +1,28 @@
+//
+//  Nokkuman.swift
+//  Nokkuman
+//
+//  Created by haru on 2020/07/08.
+//  Copyright © 2020 Harunobu Agematsu. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Nokkuman :SKSpriteNode{
+    // SKTextureで画像を指定します。
+    let mytexture = SKTexture(imageNamed: "2_entity_000_IDLE_000")
+    // キャラクターのサイズを指定します。
+    let initialSize = CGSize(width: 64, height: 64)
+    // キャラクターの位置を指定します。
+    let initialPosition = CGPoint(x: 100, y: 100)
+    
+    init() {
+        super.init(texture: mytexture, color: .clear, size: initialSize)
+        self.position = initialPosition
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
